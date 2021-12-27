743bb28e7266f868d35df0b2538e11caa2c53946
Isaque Paixao <isaquespx98@gmail.com>
2021-12-27 08:30:30 -0300

    improved

---
+++ lib/isolate_fibonacci.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+class IsolateFibonacci {
+  int? _n;
+  IsolateFibonacci(this._n);
+  Future<int> isolate() async {
+    // compute<T,R>(funcaoTopLevel).
+    //Q -> Tipo de valor de entrada.
+    //R -> Tipo de valor de resultado.
+    //funcaoTopLevel -> É a funcão que recebe o Q.
+    final result = await compute<int, int>(_fibonacci, _n!);
+    return result;
+  }
+
+  static int _fibonacci(int n) {
+    if (n < 2) {
+      return n;
+    }
+    // Fn = F(n-1) +F(n-2)
+    final fn = _fibonacci(n - 2) + _fibonacci(n - 1);
+    return fn;
+  }
+}

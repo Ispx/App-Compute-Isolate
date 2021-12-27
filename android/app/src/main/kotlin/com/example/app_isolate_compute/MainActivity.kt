04b3ad3ea90757fce4151d957dc14dd08f0b325f
Isaque Paixao <isaquespx98@gmail.com>
2021-12-27 00:07:06 -0300

    created app

---
+++ android/app/src/main/kotlin/com/example/app_isolate_compute/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_isolate_compute
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+
+import 'package:app_isolate_compute/isolate_calculate.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Compute Isolate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Isolate Fibonacci'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int n = 0;
+  Future onPressed(int n) async {
+    final isolate = IsolateCalculate(n);
+    n = await isolate.isolateCalculate();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title + ' {n=$n}'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircularProgressIndicator(),
+            const SizedBox(
+              height: 8,
+            ),
+            const Text(
+              'Calculate fibonnaci in other thread.',
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              '$n',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async => onPressed(Random().nextInt(30)),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
